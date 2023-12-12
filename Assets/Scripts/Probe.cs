4625e7e87773cde49f7a64035acbd0d05813dab1
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-12-12 17:56:53 +0300

    Probe

--- Assets/Scripts/Probe.cs
+++ Assets/Scripts/Probe.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Probe : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 5.0f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        RotateDown(); // Left (-Y)
+        //RotateLeft(); // Up (-X axis)
+        //RotateRight(); // Down (+X axis)
+        //RotateUp(); // Right (+Y)
     }
+    // Tilt - inclination or slanting.
+    void TiltProbe()
+    {
+
+    }
+    // Rotation - circular movement around an axis.
+    void RotateUp()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+    void RotateDown()
+    {
+        transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
+    }
+    void RotateLeft()
+    {
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
+    }
+    void RotateRight()
+    {
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+    }
+    // Angulation - measuring or describing angles between objects or segments.
+
 }
---
+++ Assets/Scripts/Utilis.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilis : MonoBehaviour
+{
+    public enum ChamberViews
+    {
+        Apical,
+        ParasternalLongAxis,
+        ParasternalShortAxis,
+        Subcostal,
+        Suprasternal
+    }
+}
