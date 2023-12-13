c753ecfe4d1ef4fbead7bf9d5f875ddfd7b887f8
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-12-13 22:33:47 +0300

    Probe Positions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
 
     //Adjust to the preferred positions for each chamber/probe
     public List<Vector3> probePositions;
+    public List<Vector3> probeRotations;
 
     private void Start()
     {
@@ -24,22 +25,27 @@ public class GameManager : MonoBehaviour
             case 0:
                 currentChamberView = ChamberViews.Apical;
                 probeObject.transform.position = probePositions[0];
+                probeObject.transform.position = probeRotations[0];
                 break;
             case 1:
                 currentChamberView = ChamberViews.ParasternalLongAxis;
                 probeObject.transform.position = probePositions[1];
+                probeObject.transform.position = probeRotations[1];
                 break;
             case 2:
                 currentChamberView = ChamberViews.ParasternalShortAxis;
                 probeObject.transform.position = probePositions[2];
+                probeObject.transform.position = probeRotations[2];
                 break;
             case 3:
                 currentChamberView = ChamberViews.Subcostal;
                 probeObject.transform.position = probePositions[3];
+                probeObject.transform.position = probeRotations[3];
                 break;
             case 4:
                 currentChamberView = ChamberViews.Suprasternal;
                 probeObject.transform.position = probePositions[4];
+                probeObject.transform.position = probeRotations[4];
                 break;
             default:
                 break;
